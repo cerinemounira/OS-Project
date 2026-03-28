ba0cbc3ab6286cd0266c0681b8562c19a634fab2
cerinemounira <153755465+cerinemounira@users.noreply.github.com>
2026-03-28 10:31:17 +0100

    Refactor logexec.sh for relative paths and integrity checks
    Updated script to use relative paths for report scripts and added integrity check for short report.

--- logexec.sh
+++ logexec.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
 #orchestrator of the project
 
-#variables of dir and files
 LOG_DIR="/var/log/sys_audit"
 LOG_FILE="$LOG_DIR/audit_history.log"
 SHORT_REPORT="$LOG_DIR/short_report_$(date +%Y%m%d).txt"
 FULL_REPORT="$LOG_DIR/full_report_$(date +%Y%m%d).txt"
 
-#variables of scripts
-SHORT_SCRIPT="/proscr/summary.sh"
-FULL_SCRIPT="/proscr/full.sh"
+SHORT_SCRIPT="$(dirname "$0")/summary.sh"
+FULL_SCRIPT="$(dirname "$0")/full.sh"
+MAIL_SCRIPT="$(dirname "$0")/mail.sh"
 
-#crete parent directory if not exist
-mkdir -p "$LOG_DIR" 
+#dirname gives only the directory path (.)
+#$0 gives the path of the current file (./file)
+#dirname "$0" gives the directory path of the current file or dir
+
+mkdir -p "$LOG_DIR"
 
 log_event() {
     local status="$1"
     local message="$2"
-    #like inputs for the function
     echo "$(date '+%Y-%m-%d %H:%M:%S') [$status] - $message" >> "$LOG_FILE"
 }
 
-#
 log_event "START" "Automated audit initiated."
 
-# Execute Short Report Script 
 if bash "$SHORT_SCRIPT" > "$SHORT_REPORT" 2>> "$LOG_FILE"; then
-    log_event "SUCCESS" "Short report generated via $SHORT_SCRIPT"
+    log_event "SUCCESS" "Short report generated: $SHORT_REPORT"
 else
-    log_event "ERROR" "Short report script failed execution."
+    log_event "ERROR" "Short report script failed."
 fi
 
-# Execute Full Report Script 
 if bash "$FULL_SCRIPT" > "$FULL_REPORT" 2>> "$LOG_FILE"; then
-    log_event "SUCCESS" "Full report generated via $FULL_SCRIPT"
+    log_event "SUCCESS" "Full report generated: $FULL_REPORT"
 else
-    log_event "ERROR" "Full report script failed execution."
+    log_event "ERROR" "Full report script failed."
 fi
 
-#Integrity Check 
-sha256sum "$FULL_REPORT" >> "$LOG_DIR/integrity_checks.log"
-log_event "INFO" "Integrity hash generated for full report."
+sha256sum "$FULL_REPORT" >> "$LOG_DIR/integrity_checks.log" 2>> "$LOG_FILE"
+sha256sum "$SHORT_REPORT" >> "$LOG_DIR/integrity_checks.log" 2>> "$LOG_FILE"
+log_event "INFO" "Integrity hashes generated for both reports."
+
+bash "$MAIL_SCRIPT" "$FULL_REPORT"
+log_event "INFO" "Mail script invoked for full report."
 
 log_event "FINISH" "Automation cycle complete."
