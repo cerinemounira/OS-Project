f3cab122d7821ff35c4e0afbf3422cb66772570f
cerinemounira <153755465+cerinemounira@users.noreply.github.com>
2026-04-16 08:15:47 +0100

    Refactor ssh.sh to use user input for remote details

--- ssh.sh
+++ ssh.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-# ==============================
-# Remote Report Sender Script
-# ==============================
+read -p "Enter username :" user
+read -p "Enter IP address :" ip
 
-LOG_DIR="/var/log/sys_audit"
-LOG_FILE="$LOG_DIR/audit_history.log"
+REMOTE_USER="$user"     
+REMOTE_HOST="$ip"    
 
-REMOTE_USER="user"      #replace it with real hostname 
-REMOTE_HOST="192.168.1.100"    # replace it with real ip@
-REMOTE_DIR="/home/user/reports"
+
+REMOTE_DIRS="/home/$REMOTE_USER/sshreportS.txt"
+REMOTE_DIRF="/home/$REMOTE_USER/sshreportF.txt"
 
 
 DATE=$(date +%Y%m%d)
-FULL_REPORT="$LOG_DIR/full_report_$DATE.txt"
-SHORT_REPORT="$LOG_DIR/short_report_$DATE.txt"
-
-mkdir -p "$LOG_DIR"
-
-log_event() {
-    local status="$1"
-    local message="$2"
-    echo "$(date '+%Y-%m-%d %H:%M:%S') [$status] - $message" >> "$LOG_FILE"
-}
-
-log_event "INFO" "Starting remote transfer..."
+FULL_REPORT="/var/log/sys_audit/full_report_$(date +%Y%m%d).txt"
+SHORT_REPORT="/var/log/sys_audit/short_report_$(date +%Y%m%d).txt"
 
 
-if scp "$FULL_REPORT" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"; then
-    log_event "SUCCESS" "Full report sent to $REMOTE_HOST"
+
+echo "INFO" "Starting remote transfer..."
+
+
+if scp "$FULL_REPORT" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIRF"; then
+    echo "SUCCESS" "Full report sent to $REMOTE_HOST"
 else
-    log_event "ERROR" "Failed to send full report"
+    echo "ERROR" "Failed to send full report"
 fi
 
-log_event "FINISH" "Remote transfer completed."
+
+if scp "$SHORT_REPORT" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIRS"; then
+    echo "SUCCESS" "Short report sent to $REMOTE_HOST"
+else
+    echo "ERROR" "Failed to send short report"
+fi
+
+echo "FINISH" "Remote transfer completed."
