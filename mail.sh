d78a0d5c7f9c67a1b5c96a1d327463ae30acee58
cerinemounira <153755465+cerinemounira@users.noreply.github.com>
2026-04-16 08:14:42 +0100

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
-report=$1
+REAL_USER="${SUDO_USER:-$USER}"
+REAL_HOME=$(getent passwd "$REAL_USER" | cut -d: -f6)
 
-echo "Enter your email address :"
-read EMAIL
+read -p "Enter recipient email: " EMAIL
 
-cat $report | msmtp $EMAIL 
+if [[ ! "$EMAIL" =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]];
+ then
+  echo "Invalid email format"
+  exit 1
+fi
+
+read -p "Enter a subject (leave empty as default): " SUBJECT
+if [ -z "$SUBJECT" ]; then
+  SUBJECT="System Report -- $(hostname) -- $(date)"
+fi
+
+TMPFILE=$(mktemp)
+
+{
+    echo "To: $EMAIL"
+    echo "Subject: $SUBJECT"
+    echo ""          
+
+    for file in "$@"; do
+      if [ -f "$file" ]; then
+        cat "$file"
+        echo
+      fi
+    done
+} > "$TMPFILE"
+
+
+sudo -u "$REAL_USER" msmtp "$EMAIL" < "$TMPFILE"
+
+rm -f "$TMPFILE"
